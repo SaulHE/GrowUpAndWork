6dc00e99caa4643e4662c9d775b81313e5d66f83
SaulHE <51215850+SaulHE@users.noreply.github.com>
2020-05-03 12:42:54 +0800

    Add String Id parsing methods to class Utils
    Add two methods for parsing Mount and Blade CharacterObject's string Id.

--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -1,7 +1,28 @@
-﻿namespace GrowUpAndWork.Utils
+﻿using System;
+using System.Linq;
+using GrowUpAndWorkLib.Debugging;
+
+namespace GrowUpAndWork
 {
-    public class MBStringIdToInt
+    public class Utils
     {
-        
+        public static int MBStringIdToInt(String stringId)
+        {
+            string digits = new string(stringId.Trim().SkipWhile(c => !("0123456789").Contains(c)).ToArray());
+            bool resBool = Int32.TryParse(digits, out int tempResult);
+            if (resBool)
+            {
+                // GrowthDebug.LogInfo($"The string id int number is {tempResult}");
+                return tempResult;
+            }
+            // GrowthDebug.LogInfo($"Pass Error, the error input is {stringId}");
+            return -1;
+        }
+
+        public static string MBStringIdExtractCharString(String stringId)
+        {
+            string charString = new string(stringId.Trim().SkipWhile(c => ("0123456789").Contains(c)).ToArray());
+            return charString;
+        }
     }
 }
